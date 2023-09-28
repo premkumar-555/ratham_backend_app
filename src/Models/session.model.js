3d7d25abcde9ddd26678145c4b4e214d1b0cc810
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-09-28 21:37:44 +0530

    resolved get sessions api issue for filtering expired sessions

--- src/Models/session.model.js
+++ src/Models/session.model.js
@@ -6,7 +6,7 @@ date : {type: Date, required: true},
 duration: {type: Number, default: 1},
 dean: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
 isBooked: {type: Boolean, default: false},
-student_allotted: {type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null}
+student_allotted: {type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null},
 },{
     timestamps: false
 });
@@ -15,8 +15,3 @@ const sessionModel = mongoose.model('Session', sessionSchema);
 
 module.exports = sessionModel;
 
-// {date : '2023-04-28T04:30:00.000Z'
-// duration: 1,
-// dean: "D001",
-// isBooked: false,
-// student_allotted: ''}
--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -15,21 +15,28 @@ router.post('/post_sessions', async(req, res) => {
 // get sessions 
 router.get('/get_sessions', async(req, res) => {
     try {
-        console.log('sss', req.query)
         const {dean_id} = req.query;
-        console.log(dean_id)
-        const currentDate = new Date().toISOString()
         let conditions;
         if(dean_id){
-         conditions = { $and : [{ $expr : { $gt : [ { $add : ["$date", { $multiply: [3600, 1000] } ]}, currentDate]}},
-                       {dean: dean_id}, {isBooked: true}] }
+         conditions = { $and : [ {
+                    $expr: {
+                        $gt : [ {$add : ["$date", { $multiply: ["$duration", 60*60*1000]}]}, new Date()]
+                    }
+                },
+                {dean: dean_id},
+                {isBooked: true}] }
         }else{
-            conditions = { $and : [{date: { $gt : new Date().toISOString()}},
-                       {isBooked: false} ] }
+            conditions = { $and: [
+                {
+                    $expr: {
+                        $gt : [ {$add : ["$date", { $multiply: ["$duration", 60*60*1000]}]}, new Date()]
+                    }
+                },
+                {isBooked: false}
+            ]}
         }
         // ,
         const sessions = await sessionModel.find(conditions);
-        console.log(sessions)
         return res.status(200).send(sessions)
     } catch (error) {
         return res.status(500).send(error)
@@ -40,7 +47,6 @@ router.get('/get_sessions', async(req, res) => {
 router.post('/book_session/:id', async(req, res) => {
     try {
         const id = req.params.id;
-        console.log(id)
         const {dean, date, duration, isBooked, student_allotted} = req.body;
         let missingFields = [...Object.keys(req.body).filter((ele) => (!req.body[ele]))];
         if(!dean || !date || !duration || !isBooked || !student_allotted){
